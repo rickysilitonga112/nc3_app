dd6554f5a2fe4b9a9f041b322c264fd21d9ffc63
“rickysilitonga112” <rickysilitonga112@gmail.com>
2022-07-18 20:23:27 +0700

    create picker

--- nc3_app WatchKit Extension/View/OnBoardingChooseSPF.swift
+++ nc3_app WatchKit Extension/View/OnBoardingChooseSPF.swift
@@ -8,24 +8,46 @@
 import SwiftUI
 
 struct OnBoardingChooseSPF: View {
+    let screenWidth = WKInterfaceDevice.current().screenBounds.width
+    @State private var selection = 1
+    private let spfOption: [String] = [
+        "Spf 15",
+        "Spf 20",
+        "Spf 25",
+        "Spf 30",
+    ]
+    
     var body: some View {
-        ScrollView{
-            VStack{
-                ForEach(0..<1){_ in
-                    Text("Choose your SPF Sunscreen")
-                        .frame(width: 165, alignment: .leading)
-                    
-                    
-                    
-                    Capsule()
-                        .frame(width: 184, height: 48, alignment: .center)
-                        .foregroundColor(.orange)
-                        .overlay {
-                            Text("Save")
-                        }
+        VStack {
+            Picker(selection: $selection) {
+                ForEach(0 ..< spfOption.count, id: \.self) { index in
+                    HStack {
+                        Text(spfOption[index])
+                            .tag(index)
+                            .font(index == selection ? .body : .title3)
+                    }
                 }
+
+            } label: {
+                Text("Choose your spf \nsunscreen")
+                    .frame(width: screenWidth, alignment: .leading)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.leading)
             }
+            .clipped()
+            .frame(height: 110)
+            
+            Spacer()
+            
+            Button {
+                print(selection)
+            } label: {
+                Text("Save")
+                    .foregroundColor(.white)
+            }
+            .buttonStyle(BorderedButtonStyle(tint: Color.orange.opacity(255)))
         }
+        
     }
 }
 
