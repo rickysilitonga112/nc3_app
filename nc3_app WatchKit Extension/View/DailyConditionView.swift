37b7da048dc0b547df010d9c2b75968e50c52351
“rickysilitonga112” <rickysilitonga112@gmail.com>
2022-07-18 17:06:01 +0700

    add daily condition view file

---
+++ nc3_app WatchKit Extension/View/DailyConditionView.swift
@@ -0,0 +1,20 @@
+//
+//  DailyConditionView.swift
+//  nc3_app WatchKit Extension
+//
+//  Created by Ricky Silitonga on 18/07/22.
+//
+
+import SwiftUI
+
+struct DailyConditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DailyConditionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyConditionView()
+    }
+}
