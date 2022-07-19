8357afea451d55fe388116ce83a826eb91f6912d
qrrtyn <armaqurrotaayuni@gmail.com>
2022-07-19 23:54:05 +0700

    Today UV View

--- nc3_app WatchKit Extension/View/Parent/TodayUvView.swift
+++ nc3_app WatchKit Extension/View/Parent/TodayUvView.swift
@@ -4,51 +4,96 @@
 //
 //  Created by Arma Qurrota Ayuni on 19/07/22.
 //
+//
+//  TodayUvView.swift
+//  nc3_app WatchKit Extension
+//
+//  Created by Arma Qurrota Ayuni on 19/07/22.
+//
 
 import SwiftUI
 
-struct TodayUV_View: View {
+struct UVWeather: Hashable {
+    var time : String
+    var degree : Int
+    var weather : String
+    var uvIndex : Int
+}
 
+struct TodayUVView: View {
+    
+    @State var uvWeathers = [
+        UVWeather(time: "06:00", degree: 18, weather: "sun", uvIndex: 0),
+        UVWeather(time: "07:00", degree: 18, weather: "sun", uvIndex: 2),
+        UVWeather(time: "08:00", degree: 18, weather: "sun", uvIndex: 5),
+        UVWeather(time: "09:00", degree: 18, weather: "sun", uvIndex: 10),
+        UVWeather(time: "10:00", degree: 18, weather: "sun", uvIndex: 7),
+    ]
+    
     @State var uvTime: [String] = ["06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"]
-    @State var degree: [String] = ["18","19","20", "21","22", "23", "24", "25", "26", "27", "28", "29", "30"]
-    @State var degree: [String] = ["sun"]
+    @State var degree: [String] = ["18", "19", "20", "21","22", "23", "24", "25", "26", "27", "28", "29", "30"]
+    @State var weather: [String] = ["sun"]
+    @State var uvIndex: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
     
     var body: some View {
+        //        ScrollView{
+        //            VStack{
         
-        ScrollView{
-            VStack{
-                ForEach(0 ..< 13) { index in
-                    
+        //                ForEach((0 ..< 13) .reversed(), id: \.self){ _ in
+        //                ForEach(uvTime.indices, id: \.self) { index in
+        NavigationView {
+            List(uvWeathers, id: \.self) { item in
                 HStack {
-                    Text("08.00")
+                    //                        Text(uvTime[index])
+                    Text(item.time)
                     
                     Spacer()
-                    HStack (spacing: 10) {
-      
-                        Text("25°")
-                        Image ("sun")
-                            .resizable()
-                            .frame(width: 28, height: 28)
+                    HStack {
+                        
+                        //                            Text("\(degree[index])°")
+                        Text("\(item.degree)°")
+                        //                            Image ("sun")
+                        Image(systemName: "sun.max.fill")
+                            .foregroundColor(.orange)
+                        //                                .resizable()
+                        //                                .frame(width: 28, height: 28)
                         Circle()
                             .frame(width: 22, height: 22)
-                            .foregroundColor(.green)
+                            .foregroundColor(getUVColor(uvIndex: item.uvIndex))
                             .overlay {
-                                Text("2")
+                                Text("\(item.uvIndex)")
                                     .font(.headline)
                                     .foregroundColor(.black)
                             }
                     }
                 }
-                    Divider()
-                }
-
+                .navigationTitle("Today UV")
+                .foregroundColor(.white)
+                //                    Divider()
+                //                }
+                
+                //            }
             }
         }
+    }
+    
+    func getUVColor(uvIndex: Int) -> Color {
+        if uvIndex < 3 {
+            return Color.green
+        } else if uvIndex < 6 {
+            return Color.yellow
+        } else if uvIndex < 9 {
+            return Color.orange
+        } else if uvIndex < 12 {
+            return Color.purple
+        }
+        return Color.clear
     }
 }
 
 struct TodayUvView_Previews: PreviewProvider {
     static var previews: some View {
-        TodayUvView()
+        TodayUVView()
+        
     }
 }
