f8c7e9e04a8bb74ad88bfdada810821cc8f17218
MonstaCat <andikarizkipradanaa@gmail.com>
2022-07-21 16:24:43 +0700

    Reverse geocode to convert coordinates to city name

---
+++ nc3_app WatchKit Extension/Services/GeocodeLocation.swift
@@ -0,0 +1,38 @@
+//
+//  GeocodeLocation.swift
+//  nc3_app WatchKit Extension
+//
+//  Created by Andika Rizki Pradana on 21/07/22.
+//
+
+import Foundation
+import CoreLocation
+
+class CLGeocoder : NSObject {
+	func reverseGeocodeLocation(
+		_ location: CLLocation,
+		completionHandler: @escaping CLGeocodeCompletionHandler
+	) {
+		
+		// Print and convert coordinate to city name
+		
+	}
+}
+
+//var locationManager = LocationManager()
+
+//let geoCoder = CLGeocoder()
+
+//let userLatitude = locationManager.lastLocation?.coordinate.latitude ?? 0
+//let userLongitude = locationManager.lastLocation?.coordinate.longitude ?? 0
+
+//let location = CLLocation(latitude: userLatitude, longitude: userLongitude)
+
+//geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, _) -> Void in
+//
+//	placemarks?.forEach { (placemark) in
+//
+//		if let city = placemark.locality { print(city) }
+//	}
+//})
+
